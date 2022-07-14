07b7a4cdeefe1c293773d6cd07f4479b91fd29be
Patrick Cloke <patrickc@matrix.org>
2022-07-14 13:29:02 -0400

    Include the time for the FCP to end in final comment period comment.
    Signed-off-by: Patrick Cloke <patrickc@matrix.org>

--- command_handler.py
+++ command_handler.py
@@ -368,6 +368,7 @@ class CommandHandler(object):
         comment_text = (
             f":bell: This is now entering its final comment period, "
             f"as per [the review]({status_comment.html_url}) above. :bell:"
+            f"\n\nThe final comment period will end at {fcp_conclusion_time.isoformat()}."
         )
 
         # Post a comment stating that FCP has begun
